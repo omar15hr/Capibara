7d79d75a2c1b3055e4dd466ae56814cbb908b03a
omar <omarhrn@gmail.com>
2024-05-23 11:01:55 -0400

    Agregar cantidad de productos en el carrito del icono

--- cart/urls.py
+++ cart/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.cart_detail, name='cart_detail'),
     path('add/<int:product_id>/', views.cart_add, name='cart_add'),
     path('remove/<int:product_id>/', views.cart_remove, name='cart_remove'),
-     path('cart/update_quantity/<int:product_id>/', views.update_quantity, name='cart_update_quantity'),
+    path('cart/update_quantity/<int:product_id>/', views.update_quantity, name='cart_update_quantity'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from crud.models import *
+from cart.cart import Cart
 
 def home(request):
   products = Product.objects.all()
-  return render(request, 'home.html', {'products': products})
+  cart = Cart(request)
+  cantidad_productos = len(cart)
+  return render(request, 'home.html', {'products': products, 'cart': cart, 'cantidad_productos': cantidad_productos})
 
 def category(request):
   products = Product.objects.all()
-  return render(request, 'category.html', {'products': products})
+  cart = Cart(request)
+  cantidad_productos = len(cart)
+  return render(request, 'category.html', {'products': products, 'cart': cart, 'cantidad_productos': cantidad_productos})
 
 def detalleProducto(request, productoId):
   product = get_object_or_404(Product, pk=productoId)
