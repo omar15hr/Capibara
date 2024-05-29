3d6f0ec00facaa360a78571f8ff4e45cb72aa506
omar <omarhrn@gmail.com>
2024-05-29 19:18:44 -0400

    arreglo leak

--- capibara/settings.py
+++ capibara/settings.py
@@ -138,8 +138,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_POST = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'capibaradjango@gmail.com'
-EMAIL_HOST_PASSWORD = 'capibara123'
+EMAIL_HOST_USER = '@gmail.com'
+EMAIL_HOST_PASSWORD = ''
 
 
 
